1b7ba35abf23945b27cd1253ec009b7c3c2f0056
Dok <pierredok@outlook.fr>
2023-05-31 14:25:46 +0200

    Use Layout and partials

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const hbs = require("hbs")
 
 const app = express();
 
 app.use(express.static('public')); //Make everything inside of public/ available
+
+app.set("views", __dirname + "/views");//tells our Express app where to look for our views
+app.set("view engine", "hbs");// set HBS as a the template engine
+
+hbs.registerPartials(__dirname + "/views/partials");
+
 
 
 
@@ -10,15 +17,54 @@ app.use(express.static('public')); //Make everything inside of public/ available
 // Get for /
 app.get("/", (request, res, next) => {
 
-    res.sendFile(__dirname + "/view/home-page.html");
+    res.render("home-page");
 })
 
 
 // GET /contact
 app.get('/contact', (req, res, next) => 
-
-    res.sendFile(__dirname + "/view/contact-page.html")
-
+    res.render("contact-page")
     )
+
+app.get('/pizza/margarita', (req, res, send) => {
+    
+    const info = {
+        Pizza: "Margarita",
+        Price: "14$",
+        imageFile: "pizza-margarita.jpg",
+        ingredients: ['mozzarella', 'tomato sauce', 'basilicum'],
+    }
+
+    
+    res.render("product", info)
+})
+
+app.get('/pizza/veggie', (req, res, send) => {
+    
+    const info ={
+        Pizza: "Veggie",
+        Price: '13$',
+        imageFile: "pizza-veggie.jpg",
+        ingredients: ['cherry tomatoes', 'basilicum', 'Olives'],
+    }
+
+    res.render("product", info)
+})
+
+app.get('/pizza/seafood', (req, res, send) => {
+    
+    const info = {
+        Pizza: "Seafood",
+        Price: "18$",
+        imageFile: "pizza-seafood.jpg",
+        ingredients: ['tomato sauce', 'garlic', 'prawn'],
+    }
+    
+    res.render("product", info)
+})
+
+
+
+
 
 app.listen(3000, () => console.log('My first app listening on port 3000! '));
